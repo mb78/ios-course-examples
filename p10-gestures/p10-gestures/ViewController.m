4977ab8f852f5b4dce8cd610b71e17f144f200f8
student <student@admins-iMac-3.local>
2015-01-23 15:00:48 +0100

    gestures-pan-moving-with-label

--- p10-gestures/p10-gestures/ViewController.m
+++ p10-gestures/p10-gestures/ViewController.m
@@ -11,6 +11,8 @@
 @interface ViewController () {
     
     __weak IBOutlet UILabel *label1;
+    IBOutlet UIPanGestureRecognizer *pangr1;
+    CGPoint begin;
 }
 
 @end
@@ -22,14 +24,34 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     UITapGestureRecognizer *tgr=[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapDetected:)];
-    tgr.numberOfTapsRequired=2;
+    tgr.numberOfTapsRequired=1;
     tgr.numberOfTouchesRequired=1;
     [label1 addGestureRecognizer:tgr];
+
 }
 
 -(void)tapDetected:(UITapGestureRecognizer*)obj {
     NSLog(@"tap %@",obj);
     
+}
+- (IBAction)panDetected:(UIPanGestureRecognizer*)sender {
+    NSLog(@"pan %d",sender.state);
+    if (sender.state==UIGestureRecognizerStateBegan) {
+        begin=[sender locationInView:self.view];
+        begin.x-=label1.center.x;
+        begin.y-=label1.center.y;
+        NSLog(@"pan began");
+    }
+    if (sender.state==UIGestureRecognizerStateEnded)
+        NSLog(@"pan ended");
+    if (sender.state==UIGestureRecognizerStateChanged) {
+        CGPoint pos=[sender locationInView:self.view];
+        CGPoint c=label1.center;
+        c.x=pos.x-begin.x;
+        c.y=pos.y-begin.y;
+        label1.center=c;
+        NSLog(@"pos %f,%f",pos.x,pos.y);
+    }
 }
 
 - (void)didReceiveMemoryWarning
